abe739b0779f385e7adf5404eae2b442376d3e75
Paulo Henrique <paulohenriquesa1290@icloud.com>
2025-11-09 14:19:47 -0300

    jva

--- src/Main.java
+++ src/Main.java
@@ -7,34 +7,31 @@ public class Main {
         Scanner input = new Scanner(System.in);
 
 
-
-        produtos a;
-        a = new produtos();
-
-
         System.out.println("digite o nome do produto");
-        a.nome = input.nextLine();
+        String nome = input.nextLine();
         System.out.println("digite o preço do produto");
-        a.preco = input.nextDouble();
+        double preco = input.nextDouble();
         System.out.println("digite a quantidade do produto");
-        a.quantidade = input.nextInt();
+        int quantidade = input.nextInt();
+
+        produtos a=new produtos(nome,preco,quantidade);
+
 
         System.out.println();
         System.out.println("produtos data:" + a);
 
         System.out.println();
         System.out.println("adicione mais uma quantidade no estqoe");
-        int qteentrada= input.nextInt();
+        int qteentrada = input.nextInt();
         a.entrada(qteentrada);
 
-        System.out.println("dados atualizados"+ a);
+        System.out.println("dados atualizados" + a);
 
         System.out.println(" de uma saida do estoque");
-        int qtesaida= input.nextInt();
+        int qtesaida = input.nextInt();
         a.saida(qtesaida);
 
-        System.out.println("agora seu estoque é:"+ a);
-
+        System.out.println("agora seu estoque é:" + a);
 
 
         input.close();
--- src/produtos.java
+++ src/produtos.java
@@ -1,10 +1,30 @@
 public class produtos {
-    public String nome;
-    public double preco;
-    public int quantidade;
+    private String nome;
+    private double preco;
+    private int quantidade;
+ //cosntrutor padrao
+    public produtos () {
+
+    }
 
 
+    //cosntrutor: obriga a dar entrada antes de instanciar (sobrecarga)
+    public produtos (String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
 
+   //cosntrutor apenas com nome  preço (sobrecarga)
+    public produtos(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = 0;
+
+    }
+    public void setNome(String nome){
+        this.nome= nome;
+    }
 
     public double valortotalestoque() {
         return preco * quantidade;
@@ -13,17 +33,19 @@ public class produtos {
     public void entrada(int qte) {
         quantidade = quantidade + qte;
     }
-    public void saida(int qte){
-        quantidade=quantidade-qte;
+
+    public void saida(int qte) {
+        quantidade = quantidade - qte;
     }
-    public String toString(){
+
+    public String toString() {
         return nome
                 + " , rs "
                 + String.format("%.2f", preco)
                 + " , "
                 + quantidade
                 + " unidades, total: rs"
-                +String.format(" %.2f ", valortotalestoque());
+                + String.format(" %.2f ", valortotalestoque());
 
     }
 
