5faa30c5c0744307010d2b0de4076d579806d196
Paulo Sa <paulohenriquesa1290@icloud.com>
2026-03-10 05:49:02 -0300

    adicionando try catch

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -5,17 +6,34 @@ public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner input = new Scanner(System.in);
+        produtos a=new produtos();
+        while (true){
+        try {
 
 
-        System.out.println("digite o nome do produto");
-        String nome = input.nextLine();
-        System.out.println("digite o preço do produto");
-        double preco = input.nextDouble();
-        System.out.println("digite a quantidade do produto");
-        int quantidade = input.nextInt();
+            System.out.println("digite o nome do produto");
+            String nome = input.nextLine();
+            if (nome.trim().isEmpty()) {
+                throw new IllegalArgumentException("ERRO. DIGITE UM NOME.");
+            }
+            System.out.println("digite o preço do produto");
+            double preco = input.nextDouble();
+            if (preco <= 0) {
+                throw new IllegalArgumentException("ERRO. O VALOR DEVE SER POSITIVO.");
+            }
+            System.out.println("digite a quantidade do produto");
+            int quantidade = input.nextInt();
 
-        produtos a=new produtos(nome,preco,quantidade);
+            a = new produtos(nome, preco, quantidade);
+            break;
 
+        } catch (InputMismatchException e) {
+            System.out.println("ERRO. DIGITE APENAS NUMEROS");
+            input.nextLine();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+       }
 
         System.out.println();
         System.out.println("produtos data:" + a);
